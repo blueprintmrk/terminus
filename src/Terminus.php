53bf6b1b8a6afb1c112bd3f3bebb95328b05751f
Sara McCutcheon <tesladethray@gmail.com>
2016-03-01 15:05:06 -0800

    Added command loading

--- src/Terminus.php
+++ src/Terminus.php
@@ -1,39 +1,30 @@
-#!/usr/bin/env php
 <?php
 // src/Terminus.php
-
-require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\ClassLoader\Psr4ClassLoader;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Yaml\Yaml;
 
-$terminus = new Terminus;
-$terminus();
-
-class Terminus {
+class Terminus extends Application {
 
   /*
-   * @var Application
-   */
-  private $application;
-  /*
-   * @var string
+   * @var string[]
    */
   private $command_directories = [__DIR__ . '/Commands',];
+  /**
+   * @var string
+   */
+  private $helper_directory = __DIR__ . '/Helpers';
 
   /**
-   * Terminus constructor.
-   *
-   * @return Terminus
+   * @inheritdoc
    */
   public function __construct() {
+    $this->setConstants();
     $this->setAutoloader();
-    $this->loadConstants();
-    $this->application = new Application('Terminus', TERMINUS_VERSION);
-    $this->loadCommands();
-    $this->addGlobalParams();
+    parent::__construct('Terminus', TERMINUS_VERSION);
+    $this->setCommands();
   }
 
   /**
@@ -42,16 +33,32 @@ class Terminus {
    * @throws Exception
    */
   public function __invoke () {
-    $this->application->run();
+    $this->run();
   }
 
   /**
-   * Adds Terminus global parameters to the application
-   *
-   * @return void
+   * @inheritdoc
    */
-  private function addGlobalParams() {
-    $definition = $this->application->getDefinition();
+  protected function getDefaultHelperSet() {
+    $helper_set = parent::getDefaultHelperSet();
+    $helpers    = $this->getClassInstances(
+      [
+        'directory' => $this->helper_directory,
+        'filename'  => '/^(?!\.).*Helper.php$/',
+        'classname' => '/class (.*Helper)( extends)/',
+      ]
+    );
+    foreach ($helpers as $helper) {
+      $helper_set->set($helper);
+    }
+    return $helper_set;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function getDefaultInputDefinition() {
+    $definition = parent::getDefaultInputDefinition();
     $definition->addOptions(
       [
         new InputOption(
@@ -62,62 +69,50 @@ class Terminus {
         ),
       ]
     );
+    return $definition;
   }
 
   /**
-   * Loads all commands from a directory
+   * Retrieves instances of all matching classes within a directory
    *
-   * @param string $directory Name of the directory to load commands from
-   * @return void
+   * @param string[] $arg_options Elements as follow:
+   *   string directory Directory to begin search from
+   *   string filename  Regex to match valid file names
+   *   string classname Regex to match valid class names
+   *   string namespace Regex to match valid namespaces
+   *   bool   recursive Retrieve classes from within directories
+   * @return object[]
    */
-  private function loadCommandDirectory($directory) {
-    $iterator = new DirectoryIterator($directory);
+  private function getClassInstances(array $arg_options = []) {
+    $default_options = [
+      'directory' => __DIR__, 
+      'filename'  => '/^(?!\.).*.php$/',
+      'classname' => '/class (.*) /',
+      'namespace' => '/namespace (.*);/',
+      'recursive' => true,
+    ];
+    $options         = array_merge($default_options, $arg_options);
+    $classes         = [];
+    $iterator        = new DirectoryIterator($options['directory']);
     foreach ($iterator as $file) {
       if (!$file->isDot()) {
         if ($file->isDir()) {
-          $this->loadCommandDirectory($file->getPathname()); 
-        } elseif (!empty(preg_grep('/^(?!\.).*Command.php$/', [$file->getFilename(),]))) {
+          if ($options['recursive']) {
+            $dir_options = array_merge($options, ['directory' => $file->getPathname(),]);
+            $classes     = array_merge($classes, $this->getClassInstances($dir_options)); 
+          }
+        } elseif (!empty(preg_grep($options['filename'], [$file->getFilename(),]))) {
           $contents = file_get_contents($file->getPathname());
-          preg_match("/namespace (.*);/", $contents, $namespace_matches);
-          preg_match("/class (.*Command)( extends)/", $contents, $class_matches);
+          preg_match($options['namespace'], $contents, $namespace_matches);
+          preg_match($options['classname'], $contents, $class_matches);
           if (!empty($namespace_matches) && !empty($class_matches) && !strpos($contents, 'abstract class')) {
-            include_once($file->getPathname());
             $class_name = $namespace_matches[1] . '\\' . $class_matches[1];
-            $this->application->add(new $class_name());
+            $classes[]  = new $class_name();
           }
         }
       }
     }
-  }
-
-  /**
-   * Sets the application and loads commands
-   *
-   * @return void
-   */
-  private function loadCommands() {
-    foreach($this->command_directories as $directory) {
-      $this->loadCommandDirectory($directory);
-    }
-  }
-
-  /**
-   * Loads constants from file
-   *
-   * @return void
-   */
-  private function loadConstants() {
-    $constants = Yaml::parse(
-      file_get_contents(__DIR__ . '/../config/constants.yml')
-    );
-    foreach($constants as $name => $value) {
-      if (isset($_SERVER[$name])) {
-        $env = $_SERVER[$name];
-      } elseif ($env = getenv($name)) {
-        $value = $env;
-      }
-      define($name, $value);
-    }
+    return $classes; 
   }
 
   /**
@@ -129,6 +124,45 @@ class Terminus {
     $loader = new Psr4ClassLoader();
     $loader->addPrefix('Terminus\\', __DIR__);
     $loader->register();
+  }
+
+  /**
+   * Loads all Terminus and plugin commands
+   *
+   * @return void
+   */
+  private function setCommands() {
+    foreach ($this->command_directories as $directory) {
+      $commands = $this->getClassInstances(
+        [
+          'directory' => $directory,
+          'filename'  => '/^(?!\.).*Command.php$/',
+          'classname' => '/class (.*Command)( extends)/',
+        ]
+      );
+      foreach($commands as $command) {
+        $this->add($command);
+      }
+    }
+  }
+
+  /**
+   * Loads constants from file
+   *
+   * @return void
+   */
+  private function setConstants() {
+    $constants = Yaml::parse(
+      file_get_contents(__DIR__ . '/../config/constants.yml')
+    );
+    foreach($constants as $name => $value) {
+      if (isset($_SERVER[$name])) {
+        $env = $_SERVER[$name];
+      } elseif ($env = getenv($name)) {
+        $value = $env;
+      }
+      define($name, $value);
+    }
   }
 
 }
